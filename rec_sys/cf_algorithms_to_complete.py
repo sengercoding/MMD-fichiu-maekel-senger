sheet 1, ex 2: fix mean computation and tests

rec_sys/cf_algorithms_to_complete.py
@@ -19,16 +19,24 @@ def centered_cosine_sim(
         or y.ndim > 1
     ):
         raise ValueError
-    
-    mean_x = 0
-    mean_y = 0
+
+    x_centered = x.tocsr()
+    y_centered = y.tocsr()
         
     if not centered:
-        mean_x = np.array([x.mean()] * x.shape[0])
-        mean_y = np.array([y.mean()] * y.shape[0])
+        # .mean() computes the mean over all elements.
+        mean_x = csr_array(
+            (np.array([x.sum() / x.size] * x.size), x.coords),
+            shape=x.shape,
+        )
+        # .mean() computes the mean over all elements.
+        mean_y = csr_array(
+            (np.array([y.sum() / y.size] * y.size), y.coords),
+            shape=y.shape,
+        )
     
-    x_centered = x.tocsr() - csr_array(mean_x)
-    y_centered = y.tocsr() - csr_array(mean_y)
+        x_centered -= mean_x
+        y_centered -= mean_y
 
     norm_x = np.sqrt((x_centered * x_centered).sum())
     norm_y = np.sqrt((y_centered * y_centered).sum())

rec_sys/tests.py
@@ -19,6 +19,8 @@ def test_centered_cosine_distance_neg_corr(k: int = 100, tol: float = 1e-6):
 
 def test_centered_cosine_distance_nan(k: int = 100, tol: float = 1e-6):
     x = np.array([i + 1 for i in range(k)], dtype=float)
+    y = x[::-1].copy()
+    
     for c in [2, 3, 4, 5, 6]:
         for shift in range(0, 100, 10):
             # In scipy.sparse, the missing values have to be 0.
@@ -29,14 +31,12 @@ def test_centered_cosine_distance_nan(k: int = 100, tol: float = 1e-6):
             # for opperations.
             x[c + shift] = 0
 
-    y = x[::-1]
-
     res = cfa.centered_cosine_sim(
         coo_array(x),
         coo_array(y),
     )
 
-    expected_res = 0.0311755
+    expected_res = -0.709542
     assert np.abs(res - expected_res) <= tol
 
 