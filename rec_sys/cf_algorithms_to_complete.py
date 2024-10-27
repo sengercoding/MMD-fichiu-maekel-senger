8488f10387c4415c1dbb8c37dc7e79d0d64858d5
I583825 <daniela.fichiu@sap.com>
2024-10-27 12:34:59 +0100

    add sparse implementation

--- rec_sys/cf_algorithms_to_complete.py
+++ rec_sys/cf_algorithms_to_complete.py
@@ -4,8 +4,10 @@
 import numpy as np
 from scipy.sparse import (
     coo_array,
+    csc_array,
     csr_array,
 )
+from scipy.sparse.linalg import norm
 
 def centered_cosine_sim(
     x: coo_array,
@@ -63,6 +65,97 @@ def centered_cosine_sim(
     return x_centered.dot(y_centered) / (norm_x * norm_y)
 
 
+def centered_fast_cosine_sim(
+    um: csc_array,
+    user_index: int,
+) -> np.array:
+    """Compute fast centered cosine similarity."""
+    axis = 0
+    um = csc_array(um.copy())
+
+    # Compute sums per column.
+    sums = um.sum(axis=axis)
+
+    # Compute no of non-zero entries/column.
+    ne_cols = np.diff(um.indptr)
+
+    means = sums / ne_cols
+
+    rows, cols = um.nonzero()
+    # Select means for each column.
+    means = means[cols]
+
+    user_mean = means[user_index]
+
+    # Center matrix.
+    um[(rows, cols)] -= means
+
+    centered_um = um.copy()
+
+    norms = norm(um, axis=axis)
+    norms = norms[cols]
+
+    # Centered and normalized
+    um[(rows, cols)] /= norms
+
+    user_col = um[:, [user_index]]
+
+    similarities = (um.T @ user_col).toarray().squeeze()
+
+    return centered_um, similarities, user_mean
+
+
+def rate_all_items(
+    orig_utility_matrix: csc_array,
+    user_index: int,
+    neighborhood_size: int,
+) -> None:
+    """ Compute the rating of all items not yet rated by the user"""
+    # Compute the cosine similarity between the user and all other users
+    clean_utility_matrix, similarities, user_mean = \
+        centered_fast_cosine_sim(orig_utility_matrix, user_index)
+
+    def rate_one_item(item_index: int):
+        # If the user has already rated the item, return the rating
+        if orig_utility_matrix[[item_index], [user_index]] != 0:
+            return orig_utility_matrix[[item_index], [user_index]].item()
+
+        # Find the indices of users who rated the item
+        users_who_rated = coo_array(orig_utility_matrix[[item_index], :]).coords[1]
+        # From those, get indices of users with the highest similarity (watch out: result indices are rel. to users_who_rated)
+        best_among_who_rated = np.argsort(similarities[users_who_rated])
+        # Select top neighborhood_size of them
+        best_among_who_rated = best_among_who_rated[-neighborhood_size:]
+        # Convert the indices back to the original utility matrix indices
+        best_among_who_rated = users_who_rated[best_among_who_rated]
+        # Retain only those indices where the similarity is not nan
+        best_among_who_rated = best_among_who_rated[np.isnan(similarities[best_among_who_rated]) == False]
+        if best_among_who_rated.size > 0:
+            # Compute the rating of the item
+            rating_of_item = np.round(
+                (
+                    user_mean
+                    + (
+                        np.sum(similarities[best_among_who_rated] * clean_utility_matrix[[item_index], [best_among_who_rated]])
+                        / np.sum(np.abs(similarities[best_among_who_rated]))
+                    )
+                ),
+                1,
+            )
+        else:
+            rating_of_item = np.nan
+        print(f"item_idx: {item_index}, neighbors: {best_among_who_rated}, rating: {rating_of_item}")
+        return rating_of_item
+
+    num_items = orig_utility_matrix.shape[0]
+
+    # Get all ratings
+    ratings = list(map(rate_one_item, range(num_items)))
+    return ratings
+
+    
+
+
 def center_and_nan_to_zero(matrix, axis=0):
     """ Center the matrix and replace nan values with zeros"""
     # Compute along axis 'axis' the mean of non-nan values
@@ -89,8 +182,8 @@ def fast_cosine_sim(utility_matrix, vector, axis=0):
     return scaled
 
 
-# Implement the CF from the lecture 1
-def rate_all_items(orig_utility_matrix, user_index, neighborhood_size):
+# # Implement the CF from the lecture 1
+def rate_all_items_old(orig_utility_matrix, user_index, neighborhood_size):
     print(f"\n>>> CF computation for UM w/ shape: "
           + f"{orig_utility_matrix.shape}, user_index: {user_index}, neighborhood_size: {neighborhood_size}\n")
 
